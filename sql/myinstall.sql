269d92d2aa5d6aca18a7435d5f8ffe5d01e0fc82
Andrew Wasson <awasson@lunadesign.org>
2015-01-12 14:38:10 -0800

    Install DB repair and versioning
    Fix error caused by missing “details” column in activities table .
    
    Update version info to cover CiviCRM 4.5.x

--- sql/myinstall.sql
+++ sql/myinstall.sql
@@ -17,13 +17,14 @@ CREATE  TABLE IF NOT EXISTS civi_cpd_categories (
   PRIMARY KEY (id) )
 ENGINE = InnoDB;
 
-CREATE  TABLE IF NOT EXISTS civi_cpd_activities (
-  id INT NOT NULL AUTO_INCREMENT,
-  contact_id INT NOT NULL ,
-  category_id INT NOT NULL ,
-  credit_date DATETIME NOT NULL ,
-  credits DECIMAL(6,2) NOT NULL ,
-  activity VARCHAR(45) NOT NULL ,
-  notes VARCHAR(255) NULL ,
-  PRIMARY KEY (id, contact_id, category_id) )
-ENGINE = InnoDB;
+CREATE TABLE `civi_cpd_activities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `contact_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `credit_date` datetime NOT NULL,
+  `credits` decimal(6,2) NOT NULL,
+  `activity` varchar(45) NOT NULL,
+  `details` varchar(255) DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`,`contact_id`,`category_id`)) 
+ENGINE=InnoDB
